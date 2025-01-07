8dde39cdab21a4887e69eab6fcf3028ebfaa074e
Laura-Salmonsen <balas@aarhus.dk>
2025-01-07 14:43:21 +0100

    kø = none

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -19,7 +19,7 @@ ERROR_EMAIL = "balas"
 # ----------------------
 
 # The name of the job queue (if any)
-QUEUE_NAME = "VejmanMail"
+QUEUE_NAME = None
 
 # The limit on how many queue elements to process
 MAX_TASK_COUNT = 100
